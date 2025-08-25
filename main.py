281a29c8cfd2d05c416617f02e91e4fa08e21dfb
Thiago Rodrigues <thiagorodrigues20045@gmail.com>
2025-08-25 20:36:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import random
 def escolher_dificuldade():
     print("Bem vindo ao jogo!")
     print("Por favor escolha uma dificuldade abaixo:\n")
-    print("1 - Facil (10 Tentativas) ")
-    print("2 - Medio (6 Tentativas) ")
-    print("3 - Dificil (3 Tentativas) ")
+    print("1 - Fácil (10 Tentativas) ")
+    print("2 - Médio (6 Tentativas) ")
+    print("3 - Difícil (3 Tentativas) ")
 
-    escolha = input("Escolha um numero: ")
+    escolha = input("Escolha um número: ")
     while True:
         if escolha == "1":
-            print('Voce escolheu o Modo facil')
+            print('Você escolheu o Modo facil')
             return 10
         elif escolha == "2":
-            print('Voce escolheu o Modo medio')
+            print('Você escolheu o Modo medio')
             return 6
         elif escolha == "3":
-            print('Voce escolheu o Modo dificil')
+            print('Você escolheu o Modo dificil')
             return 3
         else:
             print("Por favor digite uma escolha valida!")
@@ -29,28 +29,28 @@ def jogar():
     maximo_tentativas = escolher_dificuldade()
     tentaivas_usadas = 0
 
-    print("Adivinhe o numero de 1 - 100!")
+    print("Adivinhe o número de 1 - 100!")
 
     while tentaivas_usadas < maximo_tentativas:
         try:
             chutar = int(input(f'Tentativa {tentaivas_usadas + 1}: '))
         except ValueError:
-            print("Digite um numero valido!")
+            print("Digite um número valido!")
             continue   
 
 
         tentaivas_usadas += 1
 
         if chutar == numero_secreto:
-            print(f"Meus parabens voce acertou o numero secreto {numero_secreto} em {tentaivas_usadas} tentativas") 
+            print(f"Meus parabéns! você acertou o número secreto {numero_secreto} em {tentaivas_usadas} tentativas") 
             break
         elif chutar < numero_secreto:
-            print("O Numero e maior!\n")
+            print("O número é maior!\n")
         else:
-            print("O Numero e menor!\n")
+            print("O número é menor!\n")
 
     if chutar != numero_secreto:
-            print(f'Fim, suas tentativas acabaram! O numero era {numero_secreto}.')    
+            print(f'Fim, suas tentativas acabaram! O número era: {numero_secreto}.')    
 
 jogar()
 
